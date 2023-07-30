a048b4094bdcfbe29f9a9f7bbe5b62e00108b55c
Junior <junior.jr.jl@gmail.com>
2023-07-29 21:18:06 -0300

    feat: infraestrutura para implementar regras dos endpoints

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,4 +1,6 @@
 
+using DevFreela.Application.InputModel;
+using DevFreela.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
@@ -7,51 +9,66 @@ namespace DevFreela.API.Controllers
     public class ProjectsController : ControllerBase
     {
 
+        private readonly IProjectService _projectService;
+
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
         [HttpGet]
         public IActionResult Get(string query)
         {
-            return Ok();
+            var projects = _projectService.GetAll(query);
+            return Ok(projects);
         }
         
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            return Ok();
+            var project = _projectService.GetById(id);
+            return Ok(project);
         }
 
         [HttpPost]
-        public IActionResult Post()
+        public IActionResult Post([FromBody] NewProjectInputModel inputModel)
         {
-            return CreatedAtAction(nameof(GetById), null);
+            var project = _projectService.Create(inputModel);
+            return CreatedAtAction(nameof(GetById), project);
         }
 
         [HttpPut("{id}")]
-        public IActionResult Put()
+        public IActionResult Put(int id, [FromBody]UpdateProjectInputModel inputModel)
         {
-            return NoContent();
+            var project = _projectService.Update(id, inputModel);
+            return Ok(project);
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete()
+        public IActionResult Delete(int id)
         {
+            _projectService.Delete(id);
             return NoContent();
         }
 
         [HttpPost("{id}/comments")]
-        public IActionResult PostComment(int id)
+        public IActionResult PostComment(int id, CreateCommentInputModel inputModel)
         {
+            _projectService.CreatedComment(id, inputModel);
             return NoContent();
         }
 
         [HttpPut("{id}/start")]
         public IActionResult Start(int id)
         {
+            _projectService.Start(id);
             return NoContent();
         }
 
         [HttpPut("{id}/finish")]
         public IActionResult Finish(int id)
         {
+            _projectService.Finish(id);
             return NoContent();
         }
 
