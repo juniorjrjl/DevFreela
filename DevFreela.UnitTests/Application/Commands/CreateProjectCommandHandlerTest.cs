a57a1e9230c2d8985275501540dff81dc2000004
Junior <junior.jr.jl@gmail.com>
2024-06-01 15:03:29 -0300

    pequenos ajustes

--- DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTest.cs
+++ DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTest.cs
@@ -6,6 +6,7 @@ using DevFreela.UnitTests.Factories.Commands;
 using NSubstitute;
 using Xunit;
 using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Infrastructure.Persistence;
 
 namespace DevFreela.UnitTests.Application.Commands
 {
@@ -14,14 +15,15 @@ namespace DevFreela.UnitTests.Application.Commands
     {
 
         private readonly CreateProjectCommandHandler createProjectCommandHandler;
-        private readonly IProjectRepository projectRepository;
+
+        private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
 
         public CreateCommentCommandHandlerTest()
         {
-            projectRepository = Substitute.For<IProjectRepository>();
+            unitOfWork = Substitute.For<IUnitOfWork>();
             mapper = Substitute.For<IMapper>();
-            createProjectCommandHandler = new CreateProjectCommandHandler(projectRepository, mapper);
+            createProjectCommandHandler = new CreateProjectCommandHandler(unitOfWork, mapper);
         }
 
         [Fact]
@@ -31,13 +33,13 @@ namespace DevFreela.UnitTests.Application.Commands
             var createProjectCommand = CreateProjectCommandFactory.Instance().Generate();
             var project = ProjectFactory.Instance().Generate();
             mapper.Map<Project>(createProjectCommand).Returns(project);
-            projectRepository.AddAsync(project).Returns(Task.FromResult(project));
+            unitOfWork.ProjectRepository.AddAsync(project).Returns(Task.FromResult(project));
             // Act
             var actual = await createProjectCommandHandler.Handle(createProjectCommand, new CancellationToken());
             // Assert
             Assert.NotNull(actual);
             _ = mapper.Received().Map<Project>(Arg.Any<CreateProjectCommand>());
-            _ = projectRepository.Received().AddAsync(Arg.Any<Project>());
+            _ = unitOfWork.ProjectRepository.Received().AddAsync(Arg.Any<Project>());
         }
 
     }
